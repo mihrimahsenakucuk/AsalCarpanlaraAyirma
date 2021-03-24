34688b945d4dc4fa0953ce1cebcc3379dadf1ef1
sena <mskucukk@outlook.com>
2021-03-24 21:28:38 +0300

    asal carpanlar

---
+++ src/AsalCarpanlaraAyirma.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class AsalCarpanlaraAyirma {
+
+    public static void main(String args[]){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayınızı giriniz: ");
+        int sayi= scan.nextInt();
+
+        if(sayi > 0) {
+
+        }
+        while(sayi % 2== 0 ){
+            System.out.print(2 + " ");
+            sayi /=2 ;
+        }
+
+        for(int i = 3 ; i <= Math.sqrt(sayi); i+= 2){
+            while (sayi % i ==0) {
+                System.out.print(i + " ");
+                sayi /= i;
+            }
+        }
+        if(sayi > 2){
+            System.out.print(sayi);
+        }
+
+
+    }
+
+
+}
